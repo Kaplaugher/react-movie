fa045c9717a6e2dc8b7b72328809f31c7a460769
Kaplaugher <kaplaugher94@gmail.com>
2017-01-16 11:57:47 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+// React Stuff
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+// Custom modules
 import App from './App';
+// Custom CSS
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+
+  <Router history={hashHistory} >
+    <Route path="/" component={App} />
+    </Router>,
   document.getElementById('root')
 );
